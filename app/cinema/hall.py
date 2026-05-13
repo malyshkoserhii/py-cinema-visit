463f629571d090764cd55fbb4bf94350ac21d95a
malyshkoserhii <malyshkosergiy@gmail.com>
2026-05-13 17:57:29 +0300

    Solved

---
+++ app/cinema/hall.py
@@ -0,0 +1,22 @@
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie: str,
+            customers: list[Customer],
+            cleaning_stuff: Cleaner
+    ) -> None:
+        print(f'\"{movie}\" started in hall number {self.number}.')
+
+        for customer in customers:
+            customer.watch_movie(movie=movie)
+
+        print(f'\"{movie}\" ended.')
+
+        cleaning_stuff.clean_hall(hall_number=self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+customers_list = [
+    {"name": "Bob", "food": "Coca-cola"},
+    {"name": "Alex", "food": "popcorn"}
+]
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int, cleaner: str,
+                 movie: str) -> None:
+
+    cinema_bar = CinemaBar()
+    cinema_hall = CinemaHall(number=hall_number)
+    cleaner = Cleaner(cleaner)
+
+    guests = []
+
+    for customer in customers:
+        customer_inst = Customer(name=customer["name"], food=customer["food"])
+        cinema_bar.sell_product(customer=customer_inst,
+                                product=customer["food"])
+        guests.append(customer_inst)
+
+    cinema_hall.movie_session(movie=movie,
+                              customers=guests,
+                              cleaning_stuff=cleaner)
+
+
+cinema_visit(
+    customers=customers_list,
+    hall_number=5,
+    cleaner="Anna",
+    movie="The Martian"
+)
